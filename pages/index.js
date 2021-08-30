feb2f4153e71c686c30d5bb5c3b03de2418193ae
Alan W. Smith <alan@alanwsmith.com>
2021-08-29 22:47:06 -0400

    Moved back to clean slate

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 export default function Home() {
   return (
     <>
-      <form
-        method="post"
-        action="/api/receive-file"
-        encType="multipart/form-data"
-      >
-        <input type="file" id="testFile" name="testFile" />
-        <button type="submit">Upload</button>
-      </form>
+      <div>scratchpad</div>
     </>
   );
 }
